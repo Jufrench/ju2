4ddf56392751ed47f1a0fccdfe7618af01316ba3
Jufrench <ju.french@gmail.com>
2024-03-08 16:08:06 -0600

    Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-// const kofi_button_stroke = require("../images/kofi_button_stroke.png")
-// import icon from "../images/icon.png"
 import githubMark from "../images/github-mark.svg";
 import linkedin from "../images/linkedin.svg";
-import linkedinPng from "../images/linkedin.png";
 import email from "../images/email.svg";
 import kofi from "../images/ko-fi.svg";
 
@@ -39,14 +36,12 @@ const contactIcons = [
   {
     name: "Github",
     link: "https://www.github.com/Jufrench",
-    icon: githubMark,
-    svgDAttribute: "M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+    icon: githubMark
   },
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/julesfrench/",
-    icon: linkedin,
-    svgDAttribute: "M20,2H4C2.9,2,2,2.9,2,4v16c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M8.1,18H5V9.4h3.1V18z M6.5,8.4 L6.5,8.4C5.6,8.4,5,7.8,5,7s0.6-1.4,1.6-1.4c0.9,0,1.5,0.6,1.5,1.4S7.5,8.4,6.5,8.4z M19,18h-3.1v-4.7c0-1.3-0.8-1.6-1.1-1.6s-1.3,0.2-1.3,1.6c0,0.2,0,4.7,0,4.7h-3.1V9.4h3.1v1.2c0.4-0.7,1.2-1.2,2.7-1.2s2.7,1.2,2.7,3.9L19,18L19,18z"
+    icon: linkedin
   },
   {
     name: "Email",
@@ -100,19 +95,6 @@ const gridItemContentBody = {
   padding: "1rem",
 };
 
-// const contactStyles = {
-//   padding: "0",
-//   margin: "0",
-//   listStyleType: "none",
-// };
-
-const contacts = [
-  { name: "Github", link: "https://www.github.com/Jufrench" },
-  { name: "LinkedIn", link: "https://www.linkedin.com/in/julesfrench/" },
-  { name: "Email", link: "mailto:ju.french@gmail.com" },
-  { name: "Ko-Fi", link: "https://ko-fi.com/moijules" },
-]
-
 const pillStyles: React.CSSProperties = {
   fontSize: "0.9rem",
   padding: "6px 8px",
@@ -135,8 +117,6 @@ const rectButton: React.CSSProperties = {
   justifyContent: "center",
   alignItems: "center",
   cursor: "pointer",
-  // background: "rgba(51, 51, 51, 0.7)",
-  // color: "#fff",
   border: "none"
 }
 
@@ -158,8 +138,6 @@ interface StyledRectangleProps {
   href?: string;
 }
 
-// TODO:
-// figure out why contact buttons are smaller than project buttons
 const RectActionComp = (props: RectButtonProps): JSX.Element => {
   return (
     <>
@@ -187,14 +165,7 @@ const StyledRectangle = (props: StyledRectangleProps): JSX.Element => {
   )
 }
 
-// const svgComponent = (props: {icon: string, link: string}): JSX.Element => {
-//   return (
-    
-//   )
-// }
-
 const IndexPage: React.FC<PageProps> = () => {
-  // console.log('%cgithubMark', 'color:tomato', githubMark)
 
   return (
     <div className="content-wrapper" style={{height: "100vh", border: "2px solid red"}}>
